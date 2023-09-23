6e2c315ba7006a2a1cd4fe359b59713cd86c4f17
KayotPoziloy <yakhont04@mail.ru>
2023-09-24 00:30:21 +0300

    ver. 1.0

--- js/result.js
+++ js/result.js
@@ -1,4 +1,18 @@
 
+// // Извлекаем параметры из URL
+// const params = new URLSearchParams(window.location.search);
+// const xValue = params.get("x");
+// const yValue = params.get("y");
+// const rValue = params.get("r");
+// const result = params.get("result");
+// const currentTime = params.get("currentTime"); // Извлекаем текущее время
+// const executionTime = params.get("executionTime");
+//
+// // Добавляем результаты в таблицу
+// const table = document.querySelector("table");
+// const newRow = table.insertRow(-1);
+// newRow.innerHTML = `<td>${xValue}</td><td>${yValue}</td><td>${rValue}</td><td>${result}</td><td>${currentTime}</td><td>${executionTime}</td>`;
+
 // Извлекаем параметры из URL
 const params = new URLSearchParams(window.location.search);
 const xValue = params.get("x");
@@ -8,7 +22,51 @@ const result = params.get("result");
 const currentTime = params.get("currentTime"); // Извлекаем текущее время
 const executionTime = params.get("executionTime");
 
-// Добавляем результаты в таблицу
+// Создаем объект для текущего результата
+const currentResult = {
+    x: xValue,
+    y: yValue,
+    r: rValue,
+    result: result,
+    currentTime: currentTime,
+    executionTime: executionTime
+};
+
+// Получаем сохраненные результаты из localStorage (если они есть)
+let storedResults = localStorage.getItem('results');
+
+// Если в localStorage нет данных или они не являются массивом, создаем новый массив
+if (!storedResults || !Array.isArray(JSON.parse(storedResults))) {
+    storedResults = [];
+} else {
+    storedResults = JSON.parse(storedResults);
+}
+
+// Добавляем текущий результат в массив сохраненных результатов
+storedResults.push(currentResult);
+
+// Сохраняем обновленный массив в localStorage
+localStorage.setItem('results', JSON.stringify(storedResults));
+
+// Отображаем результаты на странице
 const table = document.querySelector("table");
-const newRow = table.insertRow(-1);
-newRow.innerHTML = `<td>${xValue}</td><td>${yValue}</td><td>${rValue}</td><td>${result}</td><td>${currentTime}</td><td>${executionTime}</td>`;
+
+// Очищаем таблицу перед добавлением новых результатов
+table.innerHTML = '<tr><th>X</th><th>Y</th><th>R</th><th>Результат</th><th>Время</th><th>Время выполнения</th></tr>';
+
+// Добавляем все результаты в таблицу
+for (const storedResult of storedResults) {
+    if (
+        storedResult.x !== null &&
+        storedResult.y !== null &&
+        storedResult.r !== null &&
+        storedResult.result !== null &&
+        storedResult.currentTime !== null &&
+        storedResult.executionTime !== null
+    ) {
+        const newRow = table.insertRow(-1);
+        newRow.innerHTML = `<td>${storedResult.x}</td><td>${storedResult.y}</td><td>${storedResult.r}</td><td>${storedResult.result}</td><td>${storedResult.currentTime}</td><td>${storedResult.executionTime}</td>`;
+    }
+}
+
+
