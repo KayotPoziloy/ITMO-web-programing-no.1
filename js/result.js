a6eccb2ff183c536f4379b5704b71ed9ef436117
KayotPoziloy <yakhont04@mail.ru>
2023-09-24 20:55:59 +0300

    ver. 1.1

--- js/result.js
+++ js/result.js
@@ -1,18 +1,3 @@
-
-// // Извлекаем параметры из URL
-// const params = new URLSearchParams(window.location.search);
-// const xValue = params.get("x");
-// const yValue = params.get("y");
-// const rValue = params.get("r");
-// const result = params.get("result");
-// const currentTime = params.get("currentTime"); // Извлекаем текущее время
-// const executionTime = params.get("executionTime");
-//
-// // Добавляем результаты в таблицу
-// const table = document.querySelector("table");
-// const newRow = table.insertRow(-1);
-// newRow.innerHTML = `<td>${xValue}</td><td>${yValue}</td><td>${rValue}</td><td>${result}</td><td>${currentTime}</td><td>${executionTime}</td>`;
-
 // Извлекаем параметры из URL
 const params = new URLSearchParams(window.location.search);
 const xValue = params.get("x");
@@ -69,4 +54,9 @@ for (const storedResult of storedResults) {
     }
 }
 
-
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", function () {
+    const table = document.querySelector("table");
+    table.innerHTML = '<tr><th>X</th><th>Y</th><th>R</th><th>Результат</th><th>Время</th><th>Время выполнения</th></tr>'; // Очищаем таблицу
+    localStorage.removeItem('results'); // Очищаем localStorage
+});
